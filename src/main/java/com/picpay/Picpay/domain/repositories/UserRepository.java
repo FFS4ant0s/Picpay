e13f91d6ddb5f3327f43f4f0bf897130e86f71a7
Fernando <silentguyband@gmail.com>
2025-06-26 17:20:04 -0300

    Services

--- src/main/java/com/picpay/Picpay/domain/repositories/UserRepository.java
+++ src/main/java/com/picpay/Picpay/domain/repositories/UserRepository.java
@@ -9,5 +9,5 @@ import com.picpay.Picpay.domain.user.User;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByDocument(String document);
     
-    Optional<User> findUserByDocument(Long id);
+    Optional<User> findUserById(Long id);
 }
